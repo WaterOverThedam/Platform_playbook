161d0b45b77d2ca22de51e6b723c1971c1b39cac
Admin <wu.yong@thelittlegym.com.cn>
2021-12-29 20:24:49 +0800

    主机初始化增加install winrm

--- host/roles/get_parameters/files/getH2_value.sh
+++ host/roles/get_parameters/files/getH2_value.sh
@@ -1,4 +1,4 @@
-/bin/bash
+#!/bin/bash
 
 h2=$(cat<<eof
 java -cp /root/winning/tmts_miniservice/modules/ccp.server/public/lib/h2*.jar org.h2.tools.Shell -url jdbc:h2:tcp://${1} -user ${2} -password ${3} -sql
